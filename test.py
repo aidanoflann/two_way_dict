0a4c00a2630c3c67a2a024247cb9f469f468ec55
aidan <aidanoflann@gmail.com>
2016-03-26 18:30:03 +0000

    did all of the stuff

---
+++ test.py
@@ -0,0 +1,66 @@
+from two_way_dict import Mapping
+import pytest
+
+
+def setup_tests():
+    global my_dict
+    my_dict = {
+        "a": 1,
+        "b": 2,
+        "c": 3
+    }
+
+
+def test_init():
+    my_mapping = Mapping(my_dict)
+
+    for key, value in my_dict.iteritems():
+        assert my_mapping.get_by_key(key) == value
+        assert my_mapping.get_by_value(value) == key
+    assert my_mapping.count() == len(my_dict)
+
+
+def test_set():
+    my_mapping = Mapping()
+
+    my_mapping.set("a", 1)
+    assert my_mapping.count() == 1
+    assert my_mapping.get_by_key("a") == 1
+    assert my_mapping.get_by_value(1) == "a"
+
+    my_mapping.set("a", 2)
+    assert my_mapping.count() == 1
+    assert my_mapping.get_by_key("a") == 2
+    assert my_mapping.get_by_value(2) == "a"
+
+    my_mapping.set("b", 2)
+    assert my_mapping.count() == 1
+    assert my_mapping.get_by_key("b") == 2
+    assert my_mapping.get_by_value(2) == "b"
+
+    with pytest.raises(ValueError):
+        my_mapping.set("b", 2)
+    assert my_mapping.count() == 1
+    assert my_mapping.get_by_key("b") == 2
+    assert my_mapping.get_by_value(2) == "b"
+
+
+def test_remove():
+    my_mapping = Mapping(my_dict)
+
+    my_mapping.remove_by_key("a")
+    assert my_mapping.count() == 2
+    assert "a" not in my_mapping.dict
+    assert 1 not in my_mapping.rdict
+
+    my_mapping.remove_by_value(2)
+    assert my_mapping.count() == 1
+    assert "b" not in my_mapping.dict
+    assert 2 not in my_mapping.rdict
+
+
+if __name__ == "__main__":
+    setup_tests()
+    test_init()
+    test_set()
+    test_remove()
--- two_way_dict.py
+++ two_way_dict.py
@@ -1,3 +1,39 @@
-class TwoWayDict():
-    def __init__(self):
-        pass
+class Mapping:
+
+    def __init__(self, input_dict=None):
+        if input_dict:
+            self.rdict = dict(zip(input_dict.values(), input_dict.keys()))
+            self.dict = input_dict
+        else:
+            self.dict = {}
+            self.rdict = {}
+
+    def set(self, key, value):
+        if key not in self.dict.keys() and value not in self.rdict.keys():
+            self.dict[key] = value
+            self.rdict[value] = key
+        elif key not in self.dict.keys() and value in self.rdict.keys():
+            self.dict.pop(self.rdict[value])
+            self.dict[key] = value
+            self.rdict[value] = key
+        elif key in self.dict.keys() and value not in self.rdict.keys():
+            self.rdict.pop(self.dict[key])
+            self.dict[key] = value
+            self.rdict[value] = key
+        else:
+            raise ValueError("Key and value both exist.")
+
+    def remove_by_key(self, key):
+        del self.rdict[self.dict.pop(key)]
+
+    def remove_by_value(self, value):
+        del self.dict[self.rdict.pop(value)]
+
+    def get_by_key(self, key):
+        return self.dict[key]
+
+    def get_by_value(self, value):
+        return self.rdict[value]
+
+    def count(self):
+        return len(self.rdict)
